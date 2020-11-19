3d349c5ea88f26d5d57ad21ee36015c1ec758677
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-11-19 21:29:05 +0100

    add inspirational quotes for lonely drinkers

--- src/Controller/ShotController.php
+++ src/Controller/ShotController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use App\Model\CocktailManager;
+use App\Model\QuoteManager;
 
 class ShotController extends AbstractController
 {
     public function show()
     {
         $cocktailManager = new CocktailManager();
-
         $cocktails = $cocktailManager->getCocktailData();
+
+        $quoteManager = new QuoteManager();
+        $quote = $quoteManager->randomQuote();
 
         return $this->twig->render('ShotGenerator/generate.html.twig', [
             'cocktails' => $cocktails,
+            'quote' => $quote,
             ]);
     }
 }
